d9c8f2dd15580626a60b66a303ae15b9865ee4d6
Lakshya Khatri <lakshyak67@gmail.com>
2022-01-12 00:30:31 +0530

    Feat: Add all Sidebar Options

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,8 +3,13 @@ import SidebarOption from './SidebarOption';
 import './Sidebar.css';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import HomeIcon from '@material-ui/icons/Home';
-import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import SearchIcon from '@material-ui/icons/Search';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookamrkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 function Sidebar() {
   return (
@@ -16,15 +21,11 @@ function Sidebar() {
       <SidebarOption  Icon={HomeIcon} text="Home"/>
       <SidebarOption  Icon={SearchIcon} text="Explore"/>
       <SidebarOption  Icon={NotificationsNoneIcon} text="Notifications"/>
-
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-
+      <SidebarOption  Icon={MailOutlineIcon} text="Messages"/>
+      <SidebarOption  Icon={BookamrkBorderIcon} text="Bookmarks"/>
+      <SidebarOption  Icon={ListAltIcon} text="Lists"/>
+      <SidebarOption  Icon={PermIdentityIcon} text="Profile"/>
+      <SidebarOption  Icon={MoreHorizIcon} text="More"/>
 
       {/* Button -> Tweet */}
     </div>
