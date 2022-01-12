324a05dc5b007e6ba2b6b06adf39ded140cb6500
Lakshya Khatri <lakshyak67@gmail.com>
2022-01-12 22:45:26 +0530

    Style: add styles for twitter icon

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -16,7 +16,7 @@ function Sidebar() {
   return (
     <div className="sidebar">
       {/* Twitter Icon */}
-      <TwitterIcon />
+      <TwitterIcon className="sidebar__twitterIcon"/>
 
       {/* SidebarOption */}
       <SidebarOption active Icon={HomeIcon} text="Home"/>
