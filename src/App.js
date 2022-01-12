e78960dab55edf833745e841fdc6c116f16b3976
Lakshya Khatri <lakshyak67@gmail.com>
2022-01-12 22:52:14 +0530

    Feat: add Feed component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import Sidebar from './Sidebar'
+import Feed from './Feed'
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World</h1>
-
-      {/* Sidebar */}
       <Sidebar />
 
       {/* Feed */}
+      <Feed />
 
       {/* Widgets */}
     </div>
---
+++ src/Feed.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+function Feed() {
+  return (
+    <h1> This is Feed </h1>
+  );
+}
+export default Feed
