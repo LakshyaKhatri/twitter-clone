1437c9df750bba69cf1d74ef75b467a340f2b6a7
Lakshya Khatri <lakshyak67@gmail.com>
2022-01-16 18:13:45 +0530

    Chore: move the app inside `src/`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,19 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+
+        {/* Modal */}
+      </main>
     </div>
   )
 }
