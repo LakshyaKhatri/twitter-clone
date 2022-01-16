876722c21e942e719e7cbde14f2c326c0c8c1b55
Lakshya Khatri <lakshyak67@gmail.com>
2022-01-17 00:26:21 +0530

    Feat: add identity option in the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -14,12 +14,12 @@ import SidebarOption from './SidebarOption'
 
 function Sidebar() {
   return (
-    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full  xl:ml-24">
-      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0">
+    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full  xl:ml-12">
+      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 mt-0">
         <Image src="https://rb.gy/ogau5a" width={30} height={30} />
       </div>
 
-      <div className="space-y-2.5 mt-4 mb-2.5">
+      <div className="mb-2.5">
         <SidebarOption text="Home" Icon={HomeIcon} active />
         <SidebarOption text="Explore" Icon={HashtagIcon} />
         <SidebarOption text="Notifications" Icon={BellIcon} />
@@ -29,8 +29,16 @@ function Sidebar() {
         <SidebarOption text="Profile" Icon={UserIcon} />
         <SidebarOption text="More" Icon={DotsCircleHorizontalIcon} />
       </div>
-
       <button className="hidden xl:inline bg-[#1d9bf0] rounded-full w-56 h-[57px] text-white text-lg font-bold shadow-md hover:bg-[#1a8cd8]">Tweet</button>
+      <div className="text-[#d9d9d9] flex items-center justify-center hoverAnimation xl:-mr-5 mt-auto">
+        <img src="https://lh3.googleusercontent.com/ogw/ADea4I5OONOW8CDeT20jW2D_l-4ulrCOKJYq_z-ZwuJS=s83-c-mo" alt="" className="h-10 w-10 rounded-full xl:mr-2.5"/>
+
+        <div className="hidden xl:inline leading-5">
+          <h4 className="font-bold">Lakshya Khatri</h4>
+          <p className="text-[#6e767d]">@lakshyakhatri_</p>
+        </div>
+        <DotsHorizontalIcon className="h-5 hidden xl:inline ml-10" />
+      </div>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+      <main className="min-h-screen flex max-w-[1500px] mx-auto">
         <Sidebar />
         {/* Feed */}
         {/* Widgets */}
