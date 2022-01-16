38ab18d5aa7444e15063bebb8bf2c43b21444936
Lakshya Khatri <lakshyak67@gmail.com>
2022-01-16 19:00:54 +0530

    Feat: add sidebar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
 import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Create Next App</title>
+      <Head className="">
+        <title>Twitter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        {/* Sidebar */}
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+        <Sidebar />
         {/* Feed */}
         {/* Widgets */}
 
