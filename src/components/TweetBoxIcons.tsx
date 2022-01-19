37127ec55166ef0d6adbe9f1337538c616d7e90a
Lakshya Khatri <lakshyak67@gmail.com>
2022-01-20 01:05:27 +0530

    Feat: add all icons in TweetBoxIcons component

--- src/components/TweetBoxIcons.tsx
+++ src/components/TweetBoxIcons.tsx
@@ -1,14 +1,31 @@
-import { PhotographIcon } from '@heroicons/react/outline'
+import {
+  CalendarIcon,
+  ChartBarIcon,
+  EmojiHappyIcon,
+  PhotographIcon
+} from '@heroicons/react/outline'
 import { useRef } from 'react'
 
 function TweetBoxIcons() {
   const filePickerRef = useRef(null);
 
   return (
-    <div className="flex items-center" onClick={() => filePickerRef.current.click()}>
-      <div className="tweet-box-icon">
+    <div className="flex items-center">
+      <div className="tweet-box-icon" onClick={() => filePickerRef.current.click()}>
         <PhotographIcon className="text-[#1d9bf0] h-[22px]" />
         <input type="file" hidden ref={filePickerRef}/>
+      </div>
+
+      <div className="tweet-box-icon rotate-90">
+        <ChartBarIcon className="text-[#1d9bf0] h-[20.5px]" />
+      </div>
+
+      <div className="tweet-box-icon">
+        <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
+      </div>
+
+      <div className="tweet-box-icon">
+        <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
       </div>
     </div>
   )
