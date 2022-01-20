c50b3bd8c269f39a7a0d0bfcc9b665eaf1afc2fa
Lakshya Khatri <lakshyak67@gmail.com>
2022-01-21 00:38:06 +0530

    Feat: add tweet button in TweetBox

--- src/components/TweetBox.tsx
+++ src/components/TweetBox.tsx
@@ -26,13 +26,19 @@ function TweetBox() {
             value={input}
             rows="2"
             placeholder="What's happening?"
-            className="mt-2 bg-transparent outline-none text-[#d9d9d9] text-lg w-full placeholder-gray-500 tracking-wide min-h-[50px]"
+            className="resize-none mt-2 bg-transparent outline-none text-[#d9d9d9] text-lg w-full placeholder-gray-500 tracking-wide min-h-[50px]"
           />
         </div>
 
         {/* Image */}
 
-        <TweetBoxIcons onEmojiClick={addEmoji} />
+        <div className="flex items-center justify-between pt-2.5">
+          <TweetBoxIcons onEmojiClick={addEmoji} />
+          <button
+            className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
+            disabled={!input.trim()}
+          >Tweet</button>
+        </div>
       </div>
     </div>
   )
