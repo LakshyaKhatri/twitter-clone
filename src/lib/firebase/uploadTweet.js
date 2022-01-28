98c25be90b31cad198ac45012933c0ee0b9d0ecc
Lakshya Khatri <lakshyak67@gmail.com>
2022-01-29 00:43:58 +0530

    Feat: add uploadTweet utility

---
+++ src/lib/firebase/uploadTweet.js
@@ -0,0 +1,32 @@
+import {
+  addDoc, collection, doc,
+  serverTimestamp, updateDoc,
+} from "@firebase/firestore"
+import { getDownloadURL, ref, uploadString } from "@firebase/storage"
+import { db, storage } from "@/firebase"
+
+async function uploadTweet(text, imageDataUrl) {
+  return new Promise(async (resolve) => {
+    const docRef = await addDoc(collection(db, "posts"), {
+      // username: session.user.name,
+      // userImg: session.user.image,
+      // tag: session.user.tag,
+      text: text,
+      timestamp: serverTimestamp(),
+    })
+
+    if (imageDataUrl) {
+      const filename = Date.now()
+      const fileExt = imageDataUrl.split(';')[0].split(':')[1].split('/')[1]
+      const imageRef = ref(storage, `posts/${filename}.${fileExt}`)
+
+      uploadString(imageRef, imageDataUrl, "data_url").then(async () => {
+        const downloadURL = await getDownloadURL(imageRef)
+        updateDoc(doc(db, "posts", docRef.id), { image: downloadURL }).then(resolve)
+      })
+    }
+    resolve()
+  })
+}
+
+export default uploadTweet
