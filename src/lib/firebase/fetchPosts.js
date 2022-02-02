4cdb84227bc68479ce8f7309b9ba375d984e987a
Lakshya Khatri <lakshyak67@gmail.com>
2022-02-02 22:57:48 +0530

    Feat: add firebase function to getch all posts

---
+++ src/lib/firebase/fetchPosts.js
@@ -0,0 +1,19 @@
+import {
+  collection, query,
+  orderBy, limit,
+  startAfter, getDocs
+} from "firebase/firestore";
+import { db } from "@/firebase"
+
+
+async function fetchPosts({pageSize, lastRecord=null}) {
+  const paginatedQuery = query(collection(db, "posts"),
+                               orderBy('created_at'),
+                               startAfter(lastRecord),
+                               limit(pageSize))
+
+  const nextSnapshot = await getDocs(paginatedQuery)
+  return nextSnapshot.docs.map(x => x.data())
+}
+
+export default fetchPosts
