6a5c9da54b63ae4bd1ad6681eaf244eb4d505d98
Lakshya Khatri <lakshyak67@gmail.com>
2022-02-07 11:25:48 +0530

    Feat: change post bg on hover for feedback

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -12,7 +12,7 @@ function Post({ postData }) {
   const { data: session } = useSession()
 
   return (
-    <div className="pt-3 pr-3 pb-1 pl-4 flex space-x-3 cursor-pointer border-b border-gray-700">
+    <div className="hover:bg-[#080808] pt-3 pr-3 pb-1 pl-4 flex space-x-3 cursor-pointer border-b border-gray-700">
       <img src={session.user.image}
            alt="profile picture"
            className="h-12 w-12 rounded-full cursor-pointer"/>
